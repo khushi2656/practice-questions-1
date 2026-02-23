0cf783601b4569108005a1cc5e9e2c735c9aa165
khushi chaturvedi <chaturvedik428@gmail.com>
2026-02-24 00:32:43 +0530

    Add reverse words in a string solution

---
+++ reversewordsinaString.java
@@ -0,0 +1,15 @@
+class Solution {
+    // method 1 - using built in functions 
+    public String reverseWords(String s) {
+       String[] words = s.trim().split("\\s+");
+       int left = 0; int right = words.length - 1;
+       while(left < right){
+        String temp = words[left];
+        words[left] =  words[right];
+        words[right] = temp;
+        left++;
+        right--;
+       }  
+       return String.join(" " , words);
+    }
+}
