4b81f96a746c11d7fae4cae167b4e967f6587373
khushi chaturvedi <chaturvedik428@gmail.com>
2026-02-24 19:35:06 +0530

    Update solutions

--- Findapeakelementll.java
+++ Findapeakelementll.java
@@ -1,29 +1,14 @@
-class Solution {
-    public int[] findPeakGrid(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int left = 0;
-        int right = n - 1;
-        while(left <= right){
-            int midCol = left + (right - left) / 2;
-            int maxRow = 0;
-            for(int i = 0; i < m; i++){
-                if(mat[i][midCol] >  mat[maxRow][midCol]){
-                    maxRow = i;
-                }
-            }
-            int leftVal = (midCol > 0) ? mat[maxRow][midCol - 1] : -1;
-            int rightVal = (midCol < n-1) ? mat[maxRow][midCol + 1]: -1;
-            int currentVal = mat[maxRow][midCol];
-
-            if(currentVal > leftVal && currentVal > rightVal){
-                return new int[]{maxRow,midCol};
-            } else if(currentVal < leftVal){
-                right = midCol - 1;
-            } else {
-                left = midCol + 1;
-            }
-        }
-        return new int[]{-1, -1};
+lass Solution {
+    public String reverseWords(String s) {
+       String[] words = s.trim().split("\\s+");
+       int left = 0; int right = words.length - 1;
+       while(left < right){
+        String temp = words[left];
+        words[left] =  words[right];
+        words[right] = temp;
+        left++;
+        right--;
+       }  
+       return String.join(" " , words);
     }
 }
