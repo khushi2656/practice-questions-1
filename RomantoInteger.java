29a23fbc3e0c6d3fe802921ed03c511012a66632
khushi chaturvedi <chaturvedik428@gmail.com>
2026-03-06 18:45:43 +0530

    Update RomantoInteger.java

--- RomantoInteger.java
+++ RomantoInteger.java
@@ -1,3 +1,33 @@
+// method 1 : using look ahead methond but forward pass only
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character , Integer> map = new HashMap<>();
+       map.put('I', 1);
+       map.put('V', 5);
+       map.put('X', 10);
+       map.put('L', 50);
+       map.put('C', 100);
+       map.put('D', 500);
+       map.put('M', 1000);
+       int result = 0;
+       for(int i = 0; i < s.length(); i++){
+        int current = map.get(s.charAt(i));
+        if(i < s.length() - 1){
+            int next = map.get(s.charAt(i + 1));
+            if(current < next){
+                result -= current;
+            } else {
+                result += current;
+            }
+        } else {
+            result += current;
+        }
+       }
+       return result;
+    }
+}
+
+// method 2 : using look ahead to determine whether to add or subtract the current value but this method is more efficient than the previous one 
 class Solution {
     public int romanToInt(String s) {
        Map<Character , Integer> map = new HashMap<>();
