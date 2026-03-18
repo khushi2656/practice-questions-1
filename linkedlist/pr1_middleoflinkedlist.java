4a592b259028a6b5c483a29d1d680158f3eeb104
khushi chaturvedi <chaturvedik428@gmail.com>
2026-03-18 17:57:27 +0530

    Update middle of linked list solution

--- linkedlist/pr1_middleoflinkedlist.java
+++ linkedlist/pr1_middleoflinkedlist.java
@@ -1,3 +1,35 @@
 package linkedlist;
 
-
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// https://leetcode.com/problems/middle-of-the-linked-list/description/
+// method 1: count the number of nodes and then find the middle node
+class Solution {
+    public ListNode middleNode(ListNode head) {
+       ListNode temp = head;
+       int count = 0;
+       while(temp != null){
+        count++;
+        temp = temp.next;
+       } 
+       int midNode = (count/2) + 1;
+       temp = head;
+       while(temp != null){
+        midNode = midNode - 1;
+        if(midNode == 0){
+            break;
+           
+        }
+         temp = temp.next;
+       }
+       return temp;
+    }
+}
