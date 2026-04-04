2fb78560bbd981037db6795cb739739b2d3d714a
khushi chaturvedi <chaturvedik428@gmail.com>
2026-04-04 18:01:48 +0530

    Add rotate list solution

---
+++ linkedlist/RotateList.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    static ListNode findNthNode(ListNode temp , int k ){
+        int cnt = 1;
+        while(temp != null){
+            if(cnt == k)return temp;
+            cnt++;
+            temp = temp.next;
+        }
+        return temp;
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || k == 0)return head;
+        ListNode tail = head;
+        int len = 1;
+        while(tail.next != null){
+            len++;
+            tail = tail.next;
+        }
+        if(k % len == 0)return head;
+        k = k % len;
+        tail.next = head;
+        ListNode newLastNode = findNthNode(head , len - k);
+        head = newLastNode.next;
+        newLastNode.next = null;
+         return head;
+         
+    }
+
+}
